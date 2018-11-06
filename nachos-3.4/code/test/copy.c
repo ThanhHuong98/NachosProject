252b9bc6ada77ca755becd70ea0d23dd7e1b8dcc
Nguyen Khang <nguyenkhacnguyenkhang@gmail.con>
2018-11-06 09:49:50 +0700

    Copy added

--- nachos-3.4/code/test/copy.c
+++ nachos-3.4/code/test/copy.c
@@ -3,13 +3,23 @@
 
 int main(int argc, char const *argv[])
 {
-    char* buf;
+    char buf[255];
     int size = 255;
-    int srcFile = Open("myFile.txt", 0);
-    int desFile = CreateFile("newww.txt");
-    PrintChar('m');
+    int srcFile;
+    int desFile;
+
+    CreateFile("newww.txt");
+    srcFile = Open("myFile.txt", 0);
+    desFile = Open("newww.txt", 0);
+    
     Read(buf, size, srcFile);
-    Halt();
+    PrintString(buf);
+    Write(buf, size, desFile);
+    
+    Close(srcFile);
+    Close(desFile);
+    PrintString("Finish!");
+    //Halt();
 
     return 0;
 }
